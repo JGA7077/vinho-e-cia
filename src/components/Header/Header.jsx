7eb5c46a8a5071efa180c54471b8b3531cc5c591
João Araújo <joaogabriel-al@hotmail.com>
2022-05-13 08:08:42 -0300

    Limpando o projeto e salvando as imagens

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../../assets/images/icones/fruit.svg'
+
+// const 
+
+const Header = () => {
+  return(
+    <header>
+      <img src={logo} alt="Logo da empresa" />
+      <h1>teste</h1>
+    </header>
+  )
+}
+
+export default Header
