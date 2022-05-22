6c6873049245f63c387f184c97d2221685b29f98
João Araújo <joaogabriel-al@hotmail.com>
2022-05-22 14:14:08 -0300

    Implementando as Rotas com as páginas já criadas

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+// import React from "react"
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import { GlobalStyle } from './components/GlobalStyle';
 import Home from './pages/Home';
+import Products from './pages/Products';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }
---
+++ src/pages/Products.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import BannerPrincipal from '../components/BannerPrincipal/BannerPrincipal';
+
+const Products = () => {
+  return(
+    <BannerPrincipal />
+  )
+}
+export default Products
